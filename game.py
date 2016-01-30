a4e8efe18feae0b8d0c024f901070ec5fbed9f56
999eagle <erik@tauchert.de>
2016-01-30 12:51:27 +0100

    next version
    - added crude saving
    - fixed some crashes if assets weren't found

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ class MainClass:
 		self.loadGameButton = Button("Spiel laden", 1, (self.width - 190) / 2, y + 100)
 		self.exitButton = Button("Beenden", 1, (self.width - 190) / 2, y + 200)
 		self.buttonIdx = 0
-		self.state = 0 # 0 - main menu, 1 - game, 2 - pause
+		self.state = 0 # 0 - main menu, 1 - game, 2 - pause, 3 - load game, 4 - save game
 	
 	def MainLoop(self):
 		bass = getCommon().getBass()
@@ -63,6 +63,30 @@ class MainClass:
 				if event.key == K_ESCAPE:
 					self.state = 1
 					self.currentScene.Unpause()
+		elif self.state == 3:
+			if event.type == pygame.KEYDOWN:
+				if event.key == K_ESCAPE:
+					self.state = 0
+		elif self.state == 4:
+			if event.type == pygame.KEYDOWN:
+				if event.key == K_ESCAPE:
+					self.state = 0
+				elif event.key == K_RETURN:
+					handle = open("save\\" + self.inputText, "w")
+					handle.write(self.loadedScene)
+					handle.close()
+					self.state = 2
+				elif event.key == K_BACKSPACE:
+					self.inputText = self.inputText[:-1]
+				elif len(self.inputText) < 20:
+					self.inputText += event.unicode
+					
+	def LoadScene(self, scene):
+		self.loadedScene = scene
+		self.currentScene = Scene.fromFile("assets\\scene\\" + scene, self.screen)
+		self.darken = pygame.Surface((self.width, self.height))
+		self.darken.fill((0, 0, 0))
+		self.darken.set_alpha(127)
 		
 	def Update(self):
 		if self.state == 0:
@@ -73,26 +97,44 @@ class MainClass:
 				sys.exit()
 			if self.newGameButton.GetState():
 				self.newGameButton.SetState(False)
-				self.currentScene = Scene.fromFile("assets\\scene\\test.txt", self.screen)
-				self.darken = pygame.Surface((self.width, self.height))
-				self.darken.fill((0, 0, 0))
-				self.darken.set_alpha(127)
+				self.LoadScene("scene1.txt")
 				self.state = 1
+			if self.loadGameButton.GetState():
+				self.loadGameButton.SetState(False)
+				_,_,files = os.walk("save\\").next()
+				self.loadSavesButtons = list()
+				y = 10
+				for file in files:
+					self.loadSavesButtons.append([Button(file, 1, 10, y), file])
+					y += 60
+				self.state = 3
 		elif self.state == 1:
 			self.currentScene.Update()
 			next = self.currentScene.GetNextScene()
 			if next:
-				self.currentScene = Scene.fromFile(next, self.screen)
+				self.LoadScene(next)
 		elif self.state == 2:
 			self.continueButton.Update()
 			self.mainMenuButton.Update()
 			self.saveButton.Update()
 			if self.mainMenuButton.GetState():
 				self.state = 0
-				return
 			if self.continueButton.GetState():
 				self.state = 1
-				return
+			if self.saveButton.GetState():
+				self.saveButton.SetState(False)
+				self.inputText = ""
+				self.state = 4
+		elif self.state == 3:
+			for b in self.loadSavesButtons:
+				b[0].Update()
+				if b[0].GetState():
+					b[0].SetState(False)
+					handle = open("save\\" + b[1], "r")
+					self.LoadScene(handle.readline().strip())
+					self.state = 1
+					handle.close()
+					
 					
 	def Render(self):
 		# TODO: draw stuff
@@ -109,6 +151,14 @@ class MainClass:
 			self.mainMenuButton.Draw(self.screen)
 			self.continueButton.Draw(self.screen)
 			self.saveButton.Draw(self.screen)
+		elif self.state == 3:
+			for b in self.loadSavesButtons:
+				b[0].Draw(self.screen)
+		elif self.state == 4:
+			ui = getCommon().getUI()
+			font = getCommon().getTextFont()
+			self.screen.blit(ui.Panel(300, 80), (10, 10))
+			self.screen.blit(font.render("File: " + self.inputText, 0, (255, 255, 255)), (20, 40))
 		pygame.display.flip()
 					
 if __name__ == "__main__":
