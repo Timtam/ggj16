d69e013628bf9f9837dab8a20924e492dd1a03a0
Toni Barth <software@satoprogs.de>
2016-01-31 13:49:26 +0100

    - all necessary stuff to create executables and packing a zip file with all needed stuff to play the game

--- accessible_output/paths.py
+++ accessible_output/paths.py
@@ -5,8 +5,8 @@ import sys
 def root(file=None):
 
  if hasattr(sys, "frozen"):
-  from win32api import GetModuleFileName
-  path = os.path.dirname(GetModuleFileName(0))
+  from script import Script
+  path = Script().Path
  else:
   path = os.path.split(os.path.realpath(__file__))[0]
 
